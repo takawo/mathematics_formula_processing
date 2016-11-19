18428766c9eea51b05eb888d3798b572b5906aa5
takawo <tkwshnsk@gmail.com>
2016-11-19 23:00:04 +0900

    add Example

---
+++ sketch_004/build/source/sketch_004.java
@@ -0,0 +1,129 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import gifAnimation.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class sketch_004 extends PApplet {
+
+/*  mathematics formula */
+//  inspired https://twitter.com/ru_sack/status/781634050163249152
+//
+//  x=(cos(3u)*cos(v))^3
+//  y=(sin(u)*cos(v))^3
+//  z=sin(v)^3
+//  -pi/2\u2266u\u2266pi/2
+//  -pi\u2266v\u2266pi
+
+
+
+// \u5909\u6570
+GifMaker gifExport;
+int gifCount = 90;
+boolean isRecord = true;
+
+// \u5909\u6570u\u306e\u6700\u5c0f\u5024
+float minU = -PI/2;
+// \u5909\u6570u\u306e\u6700\u5927\u5024
+float maxU = PI/2;
+// \u5909\u6570v\u306e\u6700\u5c0f\u5024
+float minV = -PI;
+// \u5909\u6570v\u306e\u6700\u5927\u5024
+float maxV = PI;
+// \u5206\u5272\u6570
+float div = 200;
+
+// u,v\u306e\u5897\u52a0\u30b9\u30c6\u30c3\u30d7
+float stepU = abs(maxU - minU)/div;
+float stepV = abs(maxU - minU)/div;
+
+// \u30b9\u30b1\u30fc\u30eb
+float scale = 300;
+
+// \u80cc\u666f\u3001\u30d9\u30fc\u30b9\u3001\u30ad\u30fc\u8272
+int bgColor;
+int keyColor;
+int baseColor;
+
+// setup\u95a2\u6570 : \u521d\u56de1\u5ea6\u3060\u3051\u5b9f\u884c\u3055\u308c\u308b
+public void setup() {
+   // \u30a6\u30a3\u30f3\u30c9\u30a6\u30b5\u30a4\u30ba\u3092960px,540px\u306b
+  colorMode(HSB, 360, 100, 100, 100); // HSB\u3067\u306e\u8272\u6307\u5b9a\u306b\u3059\u308b
+   // \u63cf\u753b\u3092\u6ed1\u3089\u304b\u306b
+  //hint(ENABLE_DEPTH_SORT);
+  sphereDetail(0);
+
+  bgColor = color(171, 71, 80);
+  keyColor = color(10, 95, 80);
+  baseColor = color(44, 60, 100);
+
+  gifInit();
+}
+
+// draw\u95a2\u6570 : setup\u95a2\u6570\u5b9f\u884c\u5f8c\u7e70\u308a\u8fd4\u3057\u5b9f\u884c\u3055\u308c\u308b
+public void draw() {
+  background(bgColor);
+  lights();
+  ambientLight(0, 0, 20);
+  translate(width/2, height/2, -500);
+  rotateY(map(sin(frameCount*(TWO_PI/(float)gifCount)), -1, 1, -0.65f, 0.65f));
+  rotateZ(frameCount*(TWO_PI/(float)gifCount));
+  stroke(keyColor);
+  strokeWeight(.5f);
+  line(0, -3000, 0, 0, 3000, 0);
+  noFill();
+  fill(baseColor);
+  noStroke();
+
+  for (float v = minV; v <= maxV; v += stepV) {
+    for (float u = minU; u <= maxU; u += stepU) {
+      float x = (pow(cos(3 * u) * cos(v), 3)) * scale;
+      float y = (pow(sin(u) * cos(v), 3)) * scale;
+      float z = (pow(sin(v), 3)) * scale;
+      pushMatrix();
+      translate(x, y, z);
+      sphere(5);
+      popMatrix();
+    }
+  }
+  gifDraw();
+}
+
+public void gifInit() {
+  gifExport = new GifMaker(this, getClass().getSimpleName() +".gif"); // \u30d5\u30a1\u30a4\u30eb\u540d\u306eGIF\u30a2\u30cb\u30e1\u30fc\u30b7\u30e7\u30f3\u3092\u4f5c\u6210
+  gifExport.setRepeat(0); // \u30a8\u30f3\u30c9\u30ec\u30b9\u518d\u751f
+  gifExport.setQuality(8); // \u30af\u30aa\u30ea\u30c6\u30a3(\u30c7\u30d5\u30a9\u30eb\u30c810)
+  gifExport.setDelay(33); // \u30a2\u30cb\u30e1\u30fc\u30b7\u30e7\u30f3\u306e\u9593\u9694\u309230ms(33fps)\u306b
+}
+public void gifDraw() {
+  if (isRecord == false) {
+    return;
+  }
+  //GIF\u30a2\u30cb\u30e1\u30fc\u30b7\u30e7\u30f3\u306e\u4fdd\u5b58
+  if (frameCount <= gifCount) {
+    gifExport.addFrame(); // \u30d5\u30ec\u30fc\u30e0\u3092\u8ffd\u52a0
+  } else {
+    gifExport.finish(); // \u7d42\u4e86\u3057\u3066\u30d5\u30a1\u30a4\u30eb\u4fdd\u5b58
+    exit();
+  }
+}
+  public void settings() {  size(960, 540, P3D);  smooth(); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "sketch_004" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
---
+++ sketch_004/sketch_004.pde
@@ -0,0 +1,101 @@
+/*  mathematics formula */
+//  inspired https://twitter.com/ru_sack/status/781634050163249152
+//
+//  x=(cos(3u)*cos(v))^3
+//  y=(sin(u)*cos(v))^3
+//  z=sin(v)^3
+//  -pi/2≦u≦pi/2
+//  -pi≦v≦pi
+
+import gifAnimation.*;
+
+// 変数
+GifMaker gifExport;
+int gifCount = 90;
+boolean isRecord = true;
+
+// 変数uの最小値
+float minU = -PI/2;
+// 変数uの最大値
+float maxU = PI/2;
+// 変数vの最小値
+float minV = -PI;
+// 変数vの最大値
+float maxV = PI;
+// 分割数
+float div = 200;
+
+// u,vの増加ステップ
+float stepU = abs(maxU - minU)/div;
+float stepV = abs(maxU - minU)/div;
+
+// スケール
+float scale = 300;
+
+// 背景、ベース、キー色
+color bgColor;
+color keyColor;
+color baseColor;
+
+// setup関数 : 初回1度だけ実行される
+void setup() {
+  size(960, 540, P3D); // ウィンドウサイズを960px,540pxに
+  colorMode(HSB, 360, 100, 100, 100); // HSBでの色指定にする
+  smooth(); // 描画を滑らかに
+  //hint(ENABLE_DEPTH_SORT);
+  sphereDetail(0);
+
+  bgColor = color(171, 71, 80);
+  keyColor = color(10, 95, 80);
+  baseColor = color(44, 60, 100);
+
+  gifInit();
+}
+
+// draw関数 : setup関数実行後繰り返し実行される
+void draw() {
+  background(bgColor);
+  lights();
+  ambientLight(0, 0, 20);
+  translate(width/2, height/2, -500);
+  rotateY(map(sin(frameCount*(TWO_PI/(float)gifCount)), -1, 1, -0.65, 0.65));
+  rotateZ(frameCount*(TWO_PI/(float)gifCount));
+  stroke(keyColor);
+  strokeWeight(.5);
+  line(0, -3000, 0, 0, 3000, 0);
+  noFill();
+  fill(baseColor);
+  noStroke();
+
+  for (float v = minV; v <= maxV; v += stepV) {
+    for (float u = minU; u <= maxU; u += stepU) {
+      float x = (pow(cos(3 * u) * cos(v), 3)) * scale;
+      float y = (pow(sin(u) * cos(v), 3)) * scale;
+      float z = (pow(sin(v), 3)) * scale;
+      pushMatrix();
+      translate(x, y, z);
+      sphere(5);
+      popMatrix();
+    }
+  }
+  gifDraw();
+}
+
+void gifInit() {
+  gifExport = new GifMaker(this, getClass().getSimpleName() +".gif"); // ファイル名のGIFアニメーションを作成
+  gifExport.setRepeat(0); // エンドレス再生
+  gifExport.setQuality(8); // クオリティ(デフォルト10)
+  gifExport.setDelay(33); // アニメーションの間隔を30ms(33fps)に
+}
+void gifDraw() {
+  if (isRecord == false) {
+    return;
+  }
+  //GIFアニメーションの保存
+  if (frameCount <= gifCount) {
+    gifExport.addFrame(); // フレームを追加
+  } else {
+    gifExport.finish(); // 終了してファイル保存
+    exit();
+  }
+}
