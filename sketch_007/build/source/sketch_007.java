973662ce381d3ccf367ebfd76c197c601c01c1cd
takawo <tkwshnsk@gmail.com>
2016-11-19 22:28:03 +0900

    sync Loop / Animation

--- sketch_007/build/source/sketch_007.java
+++ sketch_007/build/source/sketch_007.java
@@ -47,8 +47,7 @@ public void draw() {
   translate(width/2, height/2, -500);
   rotateX(-frameCount*(TWO_PI/(float)gifCount));
   rotateY(PI/6);
-  rotateY(map(sin(frameCount*(TWO_PI/(float)gifCount)),-1,1,PI/6+PI/6,0));
-  rotateZ(frameCount*(TWO_PI/(float)gifCount));
+  rotateZ(-frameCount*(PI/(float)gifCount));
   lights();
   fill(347,65,78);
   noStroke();
